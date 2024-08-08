678ac688fd87b8a1700fd3085fd9fcd248b2566d
Rhuan-Ribeiro <rhuan.augusto.ribeiro>
2024-08-07 21:34:17 -0300

    partes do robo

--- index.js
+++ index.js
@@ -3,7 +3,36 @@ const history = require('connect-history-api-fallback');
 
 const app = express();
 
+app.get('/api/parts', (req, res) => res.sand({
+    heads: [
+        {
+            id: 1,
+            description: 'My head description',
+            title: 'my head title',
+            src: 'api/images/head.png'
+        }
+    ],
+    bases: [
+        {
+            id: 2,
+            description: 'My base description',
+            title: 'my base title',
+            src: 'api/images/base.png'
+        }
+    ],
+    arms: [
+        {
+            id: 3,
+            description: 'My arm description',
+            title: 'my arm title',
+            src: 'api/images/arm.png'
+        }
+    ],
+}))
+
 app.use(history({index: '/index.html'}));
+
+app.use('/api/images', express.static('images'));
 
 app.use('/', express.static('dist', {index: 'index.html'}));
 
